8d137f13bc5a79da54d153ded7ca51f2d50c62e9
Bastien <bchevriaut@gmail.com>
2020-08-01 12:00:49 +0200

    restarting project with spring Initializr

---
+++ src/test/java/com/example/springMVCTutorial/SpringMvcTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springMVCTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
